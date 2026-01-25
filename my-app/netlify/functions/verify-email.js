e66bf006c8ca3c440467216032ffb29faff71bf6
Simranjeet singh <simmalhot309@gmail.com>
2026-01-25 17:24:36 +0530

    Fixed env vairables

--- my-app/netlify/functions/verify-email.js
+++ my-app/netlify/functions/verify-email.js
@@ -78,7 +78,7 @@ export async function handler(event) {
     await fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+        Authorization: `Bearer ${process?.env?.RESEND_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
